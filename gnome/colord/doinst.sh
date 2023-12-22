b3216af5b11a57e5dea97f4b486e95d539faf75e
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2023-12-22 08:39:52 -0500

    update colord slackbuild to create user/group on install

--- gnome/colord/doinst.sh
+++ gnome/colord/doinst.sh
@@ -15,3 +15,8 @@ if [ -e usr/share/glib-2.0/schemas ]; then
     /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
   fi
 fi
+
+if ! grep -q ^colord: /etc/group ; then
+  chroot . groupadd -g 303 colord
+  chroot . useradd -d /var/lib/colord -u 303 -g colord -s /bin/false colord
+fi
