17c217c9ff8753b015fe8ddd271a5ea21c12d599
mac-a-r0ni <1995739+mac-a-r0ni@users.noreply.github.com>
2024-04-10 14:18:02 -0400

    Update doinst.sh

--- gnome-extra/rhythmbox/doinst.sh
+++ gnome-extra/rhythmbox/doinst.sh
@@ -9,4 +9,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
 fi
 
 chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/ > /dev/null 2>&1
-chroot . /usr/bin/gio-querymodules @LIBDIR@/gio/modules/ > /dev/null 2>&1
