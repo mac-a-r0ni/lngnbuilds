b576ac3003186f1c4ac73ba23f6078f4f24380ee
mac-a-r0ni <1995739+mac-a-r0ni@users.noreply.github.com>
2024-05-21 10:07:20 -0400

    fixing the rage inducing issue?
    how have I been fighting flatpak for a week and Nate rando makes a change in his script which makes a click in my brain... arghhhh

--- desktop/flatpak/doinst.sh
+++ desktop/flatpak/doinst.sh
@@ -5,27 +5,27 @@ if ! grep -q ^flatpak: /etc/group ; then
 fi
 
 config() {
-  NEW="\$1"
-  OLD="\$(dirname \$NEW)/\$(basename \$NEW .new)"
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
-  if [ ! -r \$OLD ]; then
-    mv \$NEW \$OLD
-  elif [ "\$(cat \$OLD | md5sum)" = "\$(cat \$NEW | md5sum)" ]; then
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
-    rm \$NEW
+    rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 preserve_perms() {
-  NEW="\$1"
-  OLD="\$(dirname \$NEW)/\$(basename \$NEW .new)"
-  if [ -e \$OLD ]; then
-    cp -a \$OLD \${NEW}.incoming
-    cat \$NEW > \${NEW}.incoming
-    mv \${NEW}.incoming \$NEW
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
   fi
-  config \$NEW
+  config $NEW
 }
 
 config etc/profile.d/flatpak.sh.new
